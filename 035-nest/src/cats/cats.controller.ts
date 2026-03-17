dcf01262a39f43fd7fa697798e2a1687226176bd
GinWU <ginwu05@gmail.com>
2026-03-17 18:12:15 +0800

    035-nest use express, `@Query()`

--- 035-nest/src/cats/cats.controller.ts
+++ 035-nest/src/cats/cats.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import type { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(@Req() request: Request): string {
-    return (
-      JSON.stringify(request.query) + '\n' + 'this action returns all cats'
-    );
+  findAll(@Query() query: Request['query']): string {
+    return JSON.stringify(query) + '\n' + 'this action returns all cats';
   }
 }
