f47a1ac8de02c473c894980a86bcaf1d26d15500
GinWU <ginwu05@gmail.com>
2026-03-17 18:26:07 +0800

    035-nest  `@Get(':id'`

--- 035-nest/src/cats/cats.controller.ts
+++ 035-nest/src/cats/cats.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Query } from '@nestjs/common';
 import type { Request } from 'express';
+
+interface CarParam {
+  id: string;
+}
 
 @Controller('cats')
 export class CatsController {
@@ -12,5 +16,11 @@ export class CatsController {
   @Get()
   findAll(@Query() query: Request['query']): string {
     return JSON.stringify(query) + '\n' + 'this action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param() params: CarParam): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
   }
 }
