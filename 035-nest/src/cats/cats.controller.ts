afeaab2d09085a15862ac185846aca498b674202
GinWU <ginwu05@gmail.com>
2026-03-17 18:15:31 +0800

    035-nest  POST, HttpCode

--- 035-nest/src/cats/cats.controller.ts
+++ 035-nest/src/cats/cats.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
 import type { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  @HttpCode(204)
+  create(): string {
+    return 'This action returns a new cat';
+  }
+
   @Get()
   findAll(@Query() query: Request['query']): string {
     return JSON.stringify(query) + '\n' + 'this action returns all cats';
